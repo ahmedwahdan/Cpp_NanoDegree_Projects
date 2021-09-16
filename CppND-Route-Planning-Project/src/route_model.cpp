1184ae3df492ea16997b4feacdda0a72e02d86f7
Ahmed Wahdan <ahmedibrahimwahdan@gmail.com>
2021-09-16 20:24:41 +0200

    Add node to road mapping

--- CppND-Route-Planning-Project/src/route_model.cpp
+++ CppND-Route-Planning-Project/src/route_model.cpp
@@ -1,6 +1,40 @@
 #include "route_model.h"
 #include <iostream>
+#include <math.h>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+        size_t index {0};
+        for(auto node : this->Nodes())
+        {
+            RouteModel::Node _node(index, this, node);
+            m_Nodes.push_back(_node);
+            index++;
+        }
+        CreateNodeToRoadHashmap();
+}
 
+float RouteModel::Node::distance(const RouteModel::Node& otherNode) const
+{
+    float distance {0.0};
+    distance = std::sqrt(pow((x - otherNode.x), 2) + std::pow((y -otherNode.y), 2));
+    return distance;
+}
+
+void RouteModel::CreateNodeToRoadHashmap()
+{
+    for(const auto& road : Roads())
+    {
+        if(road.type != Model::Road::Type::Footway)
+        {
+            for(auto node_idx : Ways()[road.way].nodes)
+            {
+                if(node_to_road.find(node_idx) == node_to_road.end())
+                {
+                    node_to_road[node_idx] = std::vector<const Model::Road*>{};
+                }
+                node_to_road[node_idx].push_back(&road);
+            }
+        }
+
+    }
 }
--- CppND-Route-Planning-Project/src/route_model.h
+++ CppND-Route-Planning-Project/src/route_model.h
@@ -12,9 +12,16 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+        Node* parent {nullptr};
+        float h_value {std::numeric_limits<float>::max()};
+        float g_value {0.0};
+        bool visited {false};
+        std::vector<Node*> neighbors;
+
+        float distance(const RouteModel::Node& otherNode) const;
+        
       
       private:
         // Add private Node variables and methods here.
@@ -25,8 +32,21 @@ class RouteModel : public Model {
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    std::vector<Node>& SNodes ()
+    {
+      return m_Nodes;
+    }
+    std::unordered_map<int, std::vector<const Model::Road*>>const &  GetNodeToRoadMap() const
+    {
+      return node_to_road;
+    }
+
+
 
   private:
     // Add private RouteModel variables and methods here.
+   std::vector<Node> m_Nodes;
+   std::unordered_map<int, std::vector<const Model::Road*>> node_to_road;
+   void CreateNodeToRoadHashmap();
 
 };
